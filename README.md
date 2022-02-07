4546d5a4325c59b2c49536453077d0b3763a7bbf
cooper <pbg0205@naver.com>
2022-02-07 21:18:17 +0900

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # cooper-ddd-start
-ddd-start를 보고 ddd(Domain-Driven-Development) 연습
+
+DDD-START 소스 코드 연습 및 학습 정리
---
+++ src/main/java/com/cooper/cooperdddstart/CooperDddStartApplication.java
@@ -0,0 +1,13 @@
+package com.cooper.cooperdddstart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CooperDddStartApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CooperDddStartApplication.class, args);
+    }
+
+}
